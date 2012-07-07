2ad2ee00e3e0781e4277d779e11462d0d7718db3
E. Kevin Hall <hall@absinthe.org>
2012-07-07 17:36:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surgconf::Application.config.secret_token = '87a1cb25f073d735ca15b31ed4846ea74b7cfd43c06ec7f74cba376e7ee1473111da4fc5b974171f3b78c5415e1a2dcad6945aa81eb701fbc8facc10fe4fdeed'
