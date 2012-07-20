f4305df4d5e896f890b6a062ab702f4e57222710
E. Kevin Hall <hall@absinthe.org>
2012-07-19 20:21:28 -0400

    Fixed small things

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  title      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
-#
-
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :title, :password, :password_confirmation
   has_secure_password
@@ -30,3 +18,21 @@ class User < ActiveRecord::Base
       self.remember_token = SecureRandom.urlsafe_base64
     end
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  title           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean         default(FALSE)
+#  editor          :boolean         default(FALSE)
+#  viewer          :boolean         default(FALSE)
+#
+
