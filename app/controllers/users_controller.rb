65cc50dfb0dc9ca887c85bea166f8c63dd85f496
E. Kevin Hall <hall@absinthe.org>
2012-07-29 21:35:14 -0400

    small things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user,
-                only: [:index, :new, :edit, :update, :destroy, :show]
-  before_filter :editor_user, only: [:new, :create, :update, :edit]
+
   before_filter :admin_user,     only: :destroy
 
   def index
