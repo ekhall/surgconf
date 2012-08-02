5e42bf3082eefb115f8907df1ea6c90fbfde48f6
E. Kevin Hall <hall@absinthe.org>
2012-08-01 21:32:14 -0400

    Fixed user update bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       sign_in @user
       flash[:success] = "Profile updated"
