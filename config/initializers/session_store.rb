438cb985424dfde18809481ef67c0599e20e1844
E. Kevin Hall <neversphere@absinthe.org>
2013-04-26 13:11:19 -0400

    Fixed for April 2013

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Surgconf::Application.config.session_store :cookie_store, key: '_surgconf_session'
+Surgconf::Application.config.session_store :cookie_store, :key => '_surgconf_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
